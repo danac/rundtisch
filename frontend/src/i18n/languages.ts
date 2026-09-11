721574627a8c64aaf08eef12412f6064bd7404f1
dana <dana.christen@gmail.com>
2026-09-11 17:29:00 +0200

    Remove anikaelsa frontend

--- frontend/src/i18n/languages.ts
+++
@@ -1,9 +0,0 @@
-/** Supported locale codes and flag assets for the language switcher. */
-export const SUPPORTED_LANGUAGES = [
-  { code: "en", flag: "/images/flags/gb.svg" },
-  { code: "fr", flag: "/images/flags/fr.svg" },
-] as const;
-
-export type LanguageCode = (typeof SUPPORTED_LANGUAGES)[number]["code"];
-
-export const LANGUAGE_CODES = SUPPORTED_LANGUAGES.map((l) => l.code);
